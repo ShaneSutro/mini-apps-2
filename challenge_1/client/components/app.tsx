4c12ff2ed280e15aea5ac79b7fd87e3622a40983
Shane Sutro <shanesutro@gmail.com>
2021-05-25 19:39:01 -0600

    Complete retrieving information from database into state

--- challenge_1/client/components/app.tsx
+++ challenge_1/client/components/app.tsx
@@ -1,26 +1,44 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import * as ReactPaginate from 'react-paginate'
 
 interface AppProps  {
 }
 
 interface AppState {
-  test: string
+  events: [{
+    category1: string,
+    category2: string,
+    date: string,
+    description: string,
+    granularity: string,
+    lang: string
+  }?]
 }
 
 class App extends React.Component<AppProps, AppState> {
   constructor(props: {} | Readonly<{}>) {
     super(props);
-    this.state = { test: 'for something more than that' };
+    this.state = {
+      events: []
+    };
+  }
+
+  async componentDidMount() {
+    await fetch('http://localhost:3000/events')
+      .then((result) => result.json())
+      .then((records) => {
+        const newRecords: AppState['events'] = records.slice(0, 10);
+        this.setState({ events: newRecords})
+      })
+      .catch((err) => console.error(err));
   }
 
   render() {
-    const { test } = this.state;
+    const { events } = this.state;
     return (
       <h1>
         Test
-        {' '}
-        {test}
       </h1>
     );
   }
