3e96a61ba40c2fc96af54d8ff78c16e1d3e6a9c5
Shane Sutro <shanesutro@gmail.com>
2021-05-27 07:23:37 -0600

    Setup TypeScript, setup basic React app and dev server

--- challenge_2/client/components/app.tsx
+++ challenge_2/client/components/app.tsx
@@ -1,2 +1,20 @@
+import * as React from 'react';
 
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      test: '',
+    };
+  }
 
+  render() {
+    return (
+      <div>
+        <h1>Hello!</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
